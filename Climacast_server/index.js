0350f88aa14483bc8e252d7f07161cf2d76e2179
Benonking <benonkighoma@gmail.com>
2024-01-13 10:03:24 +0300

    add history end point
    Get historical data

--- Climacast_server/index.js
+++ Climacast_server/index.js
@@ -14,9 +14,7 @@ class WeatherAPI {
     this.baseUrl = process.env.baseURL;
     this.APIKey = process.env.APIKey;
     this.GeoURl = process.env.GeoURl;
-    
-    
-   
+    this.HistUrl = process.env.HistUrl
   }
     //TODO: use redis to cache cordinates Error redis Close
     // async fetchCoordinates(location) {
@@ -61,7 +59,7 @@ class WeatherAPI {
     //     throw new Error('Error fetching coordinates');
     //   }
     // }
-  async fetchCoordinates(location) {
+  async fetchCoordinates(location, date) {
     const reqs = `${this.GeoURl}?q=${location}&limit=1&appid=${this.APIKey}`;
     try {
       const res = await axios.get(reqs);
@@ -71,7 +69,7 @@ class WeatherAPI {
       return [lon, lat];
     } catch (e) {
       console.error('Error fetching coordinates:', e.message);
-      throw new Error('Error fetching coordinates');
+      //throw new Error('Error fetching coordinates');
     }
   }
 
@@ -95,17 +93,39 @@ class WeatherAPI {
     throw e;
   }
 }
+async getHistory(location, date = 'Jan 12, 21'){
+  try{
+    
+    const dateObj = new Date(date);
+    const timestamp = dateObj.getTime()
+    const [lat, lon] = await this.fetchCoordinates(location);
+    const finalTimestamp = isNaN(timestamp) ? Date.now() : timestamp;
+    console.log(lat, lon, timestamp)
+    const histurl = `${this.HistUrl}lat=${lat}&lon=${lon}&dt=${finalTimestamp}&appid=${this.APIKey}`
+    const res = await axios.get(histurl);
+    return res.data.data.weather[0].description.conditions
+  } catch (e){
+    console.log(e)
+  }
+}
 
   async fetchData(lat, lon) {
     try {
       const response = await axios.get(
         `${this.baseUrl}?lat=${lat}&lon=${lon}&units=metric&appid=${this.APIKey}`
       );
+      // forecast dictionary or object
       let forecast = {}
       for(let i= 0; i < response.data.daily.length ; i++) {
         let date = response.data.daily[i].dt
-        date =  new Date(date * 1000).toDateString();
+        date =  new Date(date * 1000).toLocaleString(
+          'en-Us', {
+          day:'2-digit',
+          year:'2-digit',
+          month:'short'
+        });
         let weather = response.data.daily[i].weather[0].description
+        //create k,v of date and coresponding weather
         forecast[date] = weather
       }
       const data = {
@@ -124,6 +144,7 @@ class WeatherAPI {
 }
 // create class instance
 const weatherAPI = new WeatherAPI();
+
 app.get('/weather/:location', async (req, res) => {
   const { location } = req.params;
   console.log(`Current weather for ${location}`)
@@ -150,14 +171,18 @@ app.get('/daily/:location', async(req, res) => {
     res.status(500).send('Error: ' + error.message);
   }
 })
-app.get('/history/:lat/:lon/:date', async (req, res) => {
-  const { lat, lon, date } = req.params;
+app.get('/history/:location/:date', async (req, res) => {
+  const { location, date } = req.params;
+  //convert date to unix time
+  //date = new Date()
   try {
-    const data = await weatherAPI.fetchData(lat, lon, new Date(date));
-    res.send(data);
+    const data = await weatherAPI.getHistory(location, date);
+    console.log(`Historical data for ${location} on ${date}`, data);
+    //set response Header
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({Historydata:data}));
   } catch (error) {
     console.log(error);
-
   }
 });
 
